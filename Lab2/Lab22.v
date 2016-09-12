ccd5b96bdf95b05363ff8e6a14c2c9e12d2dc234
Deep Vyas <vyasdeep.dv@gmail.com>
2016-09-13 02:13:43 +0530

    Exrecises for Lab2

--- Lab2/Lab22.v
+++ Lab2/Lab22.v
@@ -24,9 +24,42 @@ module FADDER(s,c,x,y,z);
     output s,c;
     wire [7:0] d;
     
-    DECODER(d,x,y,z);
+    DECODER d1(d,x,y,z);
     
     assign s = d[1]|d[2]|d[4]|d[7];
     assign c = d[3]|d[5]|d[6]|d[7];
 endmodule
-            
+
+/*=========================================Exercise========================================*/            
+module bit8_FA(sum,cout,in1,in2,cin);
+	output [7:0] sum;
+	output cout;
+	input cin;
+	input [7:0] in1,in2;
+	wire [6:0] carry_mid;
+	
+	FADDER f1(sum[0],carry_mid[0],in1[0],in2[0],cin);
+	
+	generate
+		genvar j;
+		for(j=1;j<7;j=j+1)
+			begin:fa_loop
+				FADDER f2(sum[j],carry_mid[j],in1[j],in2[j],carry_mid[j-1]);
+			end
+	endgenerate
+	
+	FADDER f3(sum[7],cout,in1[7],in2[7],carry_mid[6]);			
+endmodule
+
+module bit32_FA(sum,cout,in1,in2,cin);
+	output [31:0] sum;
+	output cout;
+	input [31:0] in1,in2;
+	input cin;
+	wire [2:0] carry_mid;
+
+	bit8_FA f1(sum[7:0],carry_mid[0],in1[7:0],in2[7:0],cin);
+	bit8_FA f2(sum[15:8],carry_mid[1],in1[15:8],in2[15:8],carry_mid[0]);
+	bit8_FA f3(sum[23:16],carry_mid[2],in1[23:16],in2[23:16],carry_mid[1]);
+	bit8_FA f4(sum[31:24],cout,in1[31:24],in2[31:24],carry_mid[2]);
+endmodule
--- Lab2/Lab23.v
+++ Lab2/Lab23.v
@@ -52,3 +52,35 @@ module comparator(signA,signB,altb,agtb,aeqb,A,B);
                aeqb =0;
            end
 endmodule
+// ======================Exercise===================================
+
+module FULLADDER(s,c,x,y,z);
+    input x,y,z;
+    output s,c;
+    reg s,c;
+    
+    always @(x or y or z)
+       {s,c} = x+y+z;
+   endmodule
+
+module ADDERSUB(c,overflow,out,A,B,sub);
+    input [3:0] A,B;
+    input sub;
+    output c,overflow;
+    output [3:0] out;
+    
+    wire c1,c2,c3;
+    wire [3:0] b_use;
+    
+    xor x1(b_use[0],B[0],sub);
+    xor x2(b_use[1],B[1],sub);
+    xor x3(b_use[2],B[2],sub);
+    xor x4(b_use[3],B[3],sub);
+        
+    FULLADDER f1(out[0],c1,A[0],B[0],sub);
+    FULLADDER f2(out[1],c2,A[0],B[0],c1);
+    FULLADDER f3(out[2],c3,A[0],B[0],c2);
+    FULLADDER f4(out[3],c,A[0],B[0],c3);
+    
+    xor(overflow,c3,c);
+endmodule    
