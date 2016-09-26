6b2193f3d5e1dc77031e5e4ed08b5f963afc9e5c
Deep Vyas <vyasdeep.dv@gmail.com>
2016-09-26 21:15:42 +0530

    32*32 regFile implementation

--- Lab5/Lab51.v
+++ Lab5/Lab51.v
@@ -70,6 +70,7 @@ module decoder2_4(register,reg_no);
 endmodule
 
 
+
 module RegFile(clk,reset,ReadReg1,ReadReg2,WriteData,WriteReg,RegWrite,ReadData1,ReadData2);
     input clk,reset,RegWrite;
     input [1:0] ReadReg1,ReadReg2,WriteReg;
@@ -97,3 +98,80 @@ module RegFile(clk,reset,ReadReg1,ReadReg2,WriteData,WriteReg,RegWrite,ReadData1
 endmodule
 
 // Extension to 32-registers coming soon!!!
+
+module bit32_1mux(out,inp,sel);
+	input [31:0] inp;
+	output out;
+	input [4:0] sel;
+	assign out = inp[sel];
+endmodule
+
+module mux32_1(RegData,reg_inp,reg_no);
+	output [31:0] RegData;
+	input [1023:0] reg_inp;
+	input [4:0] reg_no;
+	
+	generate
+		genvar j;
+		for(j=0;j<32;j=j+1)
+		begin:mux_loop
+			bit32_1mux m1(RegData[j],reg_inp[j*32+31:j*32],reg_no);
+		end
+	endgenerate
+endmodule
+
+module decoder5_32(register,reg_no);
+   output [31:0] register;
+   input [4:0] reg_no;
+   wire not0,not1;
+   reg [31:0] register;
+   always @(reg_no)
+	begin
+       register = 32'h00000000;
+       register[reg_no] =1;
+	end
+   endmodule
+module RegFile32(clk,reset,ReadReg1,ReadReg2,WriteData,WriteReg,RegWrite,ReadData1,ReadData2);
+    input clk,reset,RegWrite;
+    input [4:0] ReadReg1,ReadReg2,WriteReg;
+    input [31:0] WriteData;
+    output [31:0] ReadData1,ReadData2;
+    wire [31:0] decoder_out;
+    wire [31:0] clock_gates;
+    wire [31:0] reg_out[31:0];
+    
+   wire [1023:0] reg_pack;
+    decoder5_32 decoder(decoder_out,WriteReg);
+    
+    
+    generate
+	genvar l;
+	for(l=0;l<32;l=l+1)
+	begin:gate_loop
+ 	   and gate0(clock_gates[l],clk,RegWrite,decoder_out[l]);
+    	end
+    endgenerate
+    
+    generate
+	genvar i;
+	for(i=0;i<32;i=i+1)
+	begin:reg_loop	
+    		reg_32bit reg0(reg_out[i],WriteData,clock_gates[i],reset);
+	end
+	endgenerate
+    
+	generate
+	genvar j,k;
+	 for(k=0;k<32;k=k+1)
+	 begin
+	    for(j=0;j<32;j=j+1)
+	    begin
+		 assign reg_pack[k*32+j] = reg_out[j][k];
+	    end
+	 end
+    endgenerate
+ 
+    mux32_1 mux1(ReadData1,reg_pack,ReadReg1);
+    mux32_1 mux2(ReadData2,reg_pack,ReadReg2);
+endmodule
+// End of Exercise
