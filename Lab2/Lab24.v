a377da3408340102dffdd277cae5846da52ee37c
mayank1010 <f2014836@pilani.bits-pilani.ac.in>
2017-11-15 22:05:02 +0530

    index in FA call corrected

--- Lab2/Lab24.v
+++ Lab2/Lab24.v
@@ -22,9 +22,9 @@ module ADDERSUB(c,overflow,out,A,B,sub);
     xor x4(b_use[3],B[3],sub);
         
     FULLADDER f1(out[0],c1,A[0],B[0],sub);
-    FULLADDER f2(out[1],c2,A[0],B[0],c1);
-    FULLADDER f3(out[2],c3,A[0],B[0],c2);
-    FULLADDER f4(out[3],c,A[0],B[0],c3);
+    FULLADDER f2(out[1],c2,A[1],B[1],c1);
+    FULLADDER f3(out[2],c3,A[2],B[2],c2);
+    FULLADDER f4(out[3],c,A[3],B[3],c3);
     
     xor(overflow,c3,c);
 endmodule    
